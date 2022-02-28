30431ab0f3175da8594c31e047a193c08052839c
PoladM <poladam@code.edu.az>
2022-02-28 04:18:29 +0400

    Hamburger Menu Created

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  //catalog open close
+  $(".main-navbar__catalog-box-inner").on("click", function () {
 
-    $('.main-navbar__catalog-box-inner').on("click",function(){
-        
-        if ($( ".main-navbar__catalog-menu" ).hasClass('show')) {
-            $( ".main-navbar__catalog-menu" ).removeClass( 'show');
-        } 
-        else {
-          $( ".main-navbar__catalog-menu" ).addClass( 'show');
-        }
-    });
+    if ($(".main-navbar__catalog-menu").hasClass("show")) {
+      $(".main-navbar__catalog-menu").removeClass("show");
+    }
+     else {
+      $(".main-navbar__catalog-menu").addClass("show");
+    }
+    
   });
+
+
+});
+
+
+let hamburger = document.querySelector('.main-navbar__hamburger');
+let menu = document.querySelector('.main-navbar__mobile-menu');
+
+hamburger.addEventListener("click", function(){
+  if (menu.classList.contains('deactive')) {
+    menu.classList.remove('deactive');
+    menu.classList.add('active');
+  }
+  else{
+    menu.classList.remove('active');
+    menu.classList.add('deactive');
+  }
+})
