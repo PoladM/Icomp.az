dd1ad41e082a07d13a3b9cd66dd8936fa632847b
PoladM <poladam@code.edu.az>
2022-03-09 19:36:49 +0400

    Review Created

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,3 +92,21 @@ $('.carousel').flickity({
   autoPlay: 5000,
 });
 
+//Tab Nav
+$(document).ready(function () {
+  var pTabItem = $(".prodNav .ptItem");
+  $(pTabItem).click(function () {
+    // Tab nav active functionality
+    $(pTabItem).removeClass("active");
+    $(this).addClass("active");
+
+    // Tab container active functionality
+    var tabid = $(this).attr("id");
+    $(".prodMain").removeClass("active");
+    $("#" + tabid + "C").addClass("active");
+
+    return false;
+  });
+});
+
+
